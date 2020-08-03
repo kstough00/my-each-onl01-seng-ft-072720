6a6404c9d9c12b39c6c59810596e0186cb1d2e67
Kyle Stough <kstough00@gmail.com>
2020-08-03 19:08:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -9,7 +9,7 @@ def my_each(array)
   end
 end
 
-my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
+my_each(list)
 
 # Use the collection method 
 # To make the *return-value* the answer you want
