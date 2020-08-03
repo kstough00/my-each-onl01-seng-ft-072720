f6f27e60472d54df407340d6568b61379943604d
Kyle Stough <kstough00@gmail.com>
2020-08-03 19:06:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
-end
+list = [1, 2, 3, 4]
+
+def my_each(array)
+  i = 0
+  results = []
+  while i < array.length
+  results << yield(array[i])
+  i = i+1 
+  end
+  results
+end 
+
+my_each(list) 
