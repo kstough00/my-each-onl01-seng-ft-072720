c1bfcc0a7d656a7d60caad95fc5257cb0b7d8d47
Kyle Stough <kstough00@gmail.com>
2020-08-03 19:07:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,28 @@
-list = [1, 2, 3, 4]
+list = ["ab", "cd", "ef"]
 
 def my_each(array)
   i = 0
-  results = []
   while i < array.length
-  results << yield(array[i])
-  i = i+1 
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
   end
-  results
-end 
+end
 
-my_each(list) 
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
+
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
